14fd3a0d05e666e892fed6b3268035432edea7a3
Bill Long (CPR) <bilong@microsoft.com>
2018-11-20 17:11:01 -0600

    First commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using MimeKit;
+using Heijden.DNS;
+
+namespace dkimstuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = MimeMessage.Load("D:\\cases\\Chinese\\SSCOWBEXETDR02B_Edge\\Original.eml");
+            var locator = new DkimPublicKeyLocator();
+            var index = message.Headers.IndexOf(HeaderId.DkimSignature);
+
+            if (index == -1)
+            {
+                Console.WriteLine("NO SIGNATURE");
+            }
+
+            var dkim = message.Headers[index];
+
+            if (message.Verify(dkim, locator))
+            {
+                // the DKIM-Signature header is valid!
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("VALID");
+                Console.ResetColor();
+            }
+            else
+            {
+                // the DKIM-Signature is invalid!
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("INVALID");
+                Console.ResetColor();
+            }
+        }
+    }
+}
